90c876ce1acb45253cbbaa50393a7cb8f8597fb9
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-07-27 12:13:12 +0300

    Added a router link inorder for user to navigate through my app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Patient } from './patient';
-import { PatientService } from './patient.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,91 +6,11 @@ import { PatientService } from './patient.service';
   styleUrls: ['./app.component.css'],
   template: `
   <h1>{{title}}</h1>
-  <h2>My Patients</h2>
-  <ul class="patients">
-  <!--To select a patient in order to see their details-->
-  <li *ngFor="let patient of patients" (click)="onSelect(patient)" [class.selected]="patient=== selectedPatient">
-  <!--each patient goes here-->
-  <span class="badge">{{patient.id}}</span> {{patient.firstName}}, {{patient.lastName}}, {{patient.dOB}}, {{patient.telephone}}, {{patient.emailAddress}}
-  </li>
-  </ul>
-  <patient-detail [patient]="selectedPatient"></patient-detail>
+  <a routerLink="/patients">Patients</a>
+  <router-outlet></router-outlet>
   `,
-  styles: [`
-  .selected {
-    background-color: #CFD8DC !important;
-    color: white;
-  }
-  .patients {
-    margin: 0 0 2em 0;
-    list-style-type: none;
-    padding: 0;
-    width: 40em;
-  }
-  .patients li {
-    cursor: pointer;
-    position: relative;
-    left: 0;
-    background-color: #EEE;
-    margin: .5em;
-    padding: .3em 0;
-    height: 1.6em;
-    border-radius: 4px;
-  }
-  .patients li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .patients li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .patients .text {
-    position: relative;
-    top: -3px;
-  }
-  .patients .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }
-`],
-providers: [PatientService]
 })
-export class AppComponent implements OnInit {
+
+export class AppComponent {
   title = 'Health E-Net';
-  // patients = PATIENTS;
-  patients: Patient[];
-  selectedPatient: Patient;
-  constructor(private patientService: PatientService) { }
-
-  getPatients(): void {
-  this.patientService.getPatients().then(patients => this.patients = patients);
-  }
-
-  ngOnInit():void {
-    this.getPatients();
-  }
-
-  onSelect(patient: Patient): void {
-    this.selectedPatient = patient;
-  }
-  // patient: Patient = {
-  //   id: 1,
-  //   firstName: 'Douglas',
-  //   lastName: 'Hero',
-  //   dOB: '22nd January 1990',
-  //   telephone: '(+254) 700 123 456',
-  //   emailAddress: 'herodougie@gmail.com'
-  // };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PatientDetailComponent } from './patient-detail.component';
+import { PatientsComponent } from './patients.component';
+import { PatientService } from './patient.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     PatientDetailComponent,
+    PatientsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
-
+    RouterModule.forRoot([
+  {
+    path: 'heroes',
+    component: PatientsComponent
+  }
+])
   ],
   providers: [],
   bootstrap: [AppComponent]
