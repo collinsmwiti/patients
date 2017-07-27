6161ffaf56b7210631c54c855adb486ca748ede5
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-07-27 10:36:02 +0300

    I have styled the app, added a click listener to expose the selected patient through onSelect method, and included ngIf to hide the empty details when one refreshes the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,44 +24,96 @@ const PATIENTS: Patient[] = [
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   template: `
+  <h1>{{title}}</h1>
   <h2>My Patients</h2>
   <ul class="patients">
-  <li *ngFor="let patient of patients">
+  <li *ngFor="let patient of patients" (click)="onSelect(patient)" [class.selected]="patient=== selectedPatient">
   <!--each patient goes here-->
   <span class="badge">{{patient.id}}</span> {{patient.firstName}}, {{patient.lastName}}, {{patient.dOB}}, {{patient.telephone}}, {{patient.emailAddress}}
   </li>
   </ul>
-  <h1>{{title}}</h1>
-  <h2>{{patient.firstName}} details!</h2>
+  <div *ngIf="selectedPatient">
+  <h2>{{selectedPatient.firstName}} details!</h2>
   <div>
   <label>id: </label>
-  {{patient.id}}
+  {{selectedPatient.id}}
   </div>
   <div>
   <label>FirstName: </label>
-  <input [(ngModel)]="patient.firstName" placeholder="FirstName">
+  <input [(ngModel)]="selectedPatient.firstName" placeholder="FirstName">
   </div>
   <div>
   <label>LastName: </label>
-  <input [(ngModel)]="patient.lastName" placeholder="LastName">
+  <input [(ngModel)]="selectedPatient.lastName" placeholder="LastName">
   </div>
   <div>
   <label>DOB: </label>
-  <input [(ngModel)]="patient.dOB" placeholder="DOB">
+  <input [(ngModel)]="selectedPatient.dOB" placeholder="DOB">
   </div>
   <div>
   <label>Telephone: </label>
-  <input [(ngModel)]="patient.telephone" placeholder="Telephone">
+  <input [(ngModel)]="selectedPatient.telephone" placeholder="Telephone">
   </div>
   <div>
   <label>EmailAddress:</label>
-  <input [(ngModel)]="patient.emailAddress" placeholder="EmailAddress">
+  <input [(ngModel)]="selectedPatient.emailAddress" placeholder="EmailAddress">
   </div>
-  `
+  </div>
+  `,
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .patients {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 40em;
+  }
+  .patients li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .patients li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .patients li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .patients .text {
+    position: relative;
+    top: -3px;
+  }
+  .patients .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
 export class AppComponent {
   title = 'Health E-Net';
   patients = PATIENTS;
+  selectedPatient: Patient;
   // patient: Patient = {
   //   id: 1,
   //   firstName: 'Douglas',
