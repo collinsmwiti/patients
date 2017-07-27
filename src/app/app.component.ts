5cf828ec67e043190819f46172d60d5a6e38774e
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-07-27 08:41:36 +0300

    Created my first patient as an object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+//class Patient
+export class Patient {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{patient.name}} details!</h2>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Health E-Net';
+  patient: Patient = {
+    id: 1,
+    name: 'Douglas'
+  };
 }
