9c6a1139504c3ecea3ec106b595904bb83cc4476
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-07-28 11:55:34 +0300

    Ammended dashboard route, also, the user can view patients within the app

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Patient } from './patient';
+import { PatientService } from './patient.service';
 
 @Component({
   selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
+  templateUrl:'./dashboard.component.html',
 })
 
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit{
+  patients: Patient[] = [];
+
+  constructor(private patientService: PatientService) { }
+
+  ngOnInit(): void {
+    this.patientService.getPatients().then(patients => this.patients = patients.slice(1, 5));
+  }
+ }
